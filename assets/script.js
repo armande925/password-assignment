ad08c75b20035103fd7f609711403eff597f1734
armande925 <armande.milhouse@gmail.com>
2020-01-12 20:44:12 -0800

    First draft of the week three homework. Have most of the final code done. Next push will be asthetics

---
+++ assets/script.js
@@ -0,0 +1,44 @@
+// Request length of password to be generated
+let passwordLength = parseInt( prompt("Enter the length of the password"));
+// Request lowercase/uppercase/numeric/special characters boolean value
+let lowerCase = confirm("Would you like to have lowercase in your password?");
+let upperCase = confirm("Would you like to have uppercase in your password?");
+let numeric = confirm("Would you like to have numbers in your password?");
+let specialChar = confirm("Would you like to have special characters in your password?");
+// Generate Password
+let lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz"
+let upperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+let numericCharacters = "0123456789"
+let specialCharCharacters = "!@#$%^&*_+"
+let validCharacters = ""
+let password = ""
+if (lowerCase) {
+    validCharacters += lowerCaseCharacters
+
+}
+if (upperCase) {
+    validCharacters += upperCaseCharacters
+    
+}
+if (numeric) {
+    validCharacters += numericCharacters
+    
+}
+if (specialChar) {
+    validCharacters += specialCharCharacters
+    
+}
+for (let i=0; i<passwordLength; i++) {
+    let index = Math.floor(Math.random()*validCharacters.length);
+    password += validCharacters[index]
+}
+console.log("Password",password)
+// Display it on the html page
+document.getElementById("passwordtextfield").innerHTML=password
+
+function copypassword() {
+    let passwordText = document.querySelector("#passwordtextfield")
+    passwordText.select()
+    document.execCommand("copy")
+    alert("Your Password Has Been Copied")
+}
